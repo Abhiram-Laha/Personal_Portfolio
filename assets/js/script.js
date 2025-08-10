92eb619e018200da64e98a27832e34945eed3594
Abhiram-Laha <abhiramlaha2020@gmail.com>
2025-08-10 13:11:21 +0530

    after passout

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["", "web development" , "machine learning","Java Development"],
+    strings: ["","cyber Security" ,"web development" , "machine learning"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
